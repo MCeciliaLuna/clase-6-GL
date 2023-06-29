e292835e5341ad598042c7b73025185e3dbaa07a
MCeciliaLuna <lunama.cecilia@live.com>
2023-06-29 16:15:55 -0300

    correcciones de código hechas

--- script.js
+++ script.js
@@ -7,31 +7,34 @@ const inputBirthday = document.getElementById("birthday");
 const inputNewsletter = document.getElementById("check-newsletter");
 const buttonSubmit = document.getElementById("submit");
 
+const regexName = new RegExp(/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/);
+const regexUsername = new RegExp(/^[A-Za-z]\w{5,29}$/);
+
 try {
   inputFirstName.addEventListener("change", () => {
     const value = inputFirstName.value.trim();
-    const regex = /^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/;
-  
+    const regex = regexName;
+
     if (!regex.test(value)) {
       alert("Debes ingresar solamente texto, no números ni símbolos");
       inputFirstName.value = "";
     }
   });
-  
+
   inputLastName.addEventListener("change", () => {
     const value = inputLastName.value.trim();
-    const regex = /^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/;
-  
+    const regex = regexName;
+
     if (!regex.test(value)) {
       alert("Debes ingresar solamente texto, no números ni símbolos");
       inputLastName.value = "";
     }
   });
-  
+
   inputUsername.addEventListener("change", () => {
     const value = inputUsername.value.trim();
-    const regex = /^[A-Za-z]\w{5,29}$/;
-  
+    const regex = regexUsername;
+
     if (!regex.test(value)) {
       alert(
         "Sólo puedes agregar letras, números y _ , sin espacios ni otros símbolos"
@@ -39,18 +42,18 @@ try {
       inputUsername.value = "";
     }
   });
-  
+
   const testPasswords = () => {
     const firstPassword = inputPassword.value;
     const secondPassword = inputConfirmPassword.value;
-  
+
     if (firstPassword !== secondPassword) {
       alert("Las contraseñas no coinciden");
       inputConfirmPassword.value = "";
     }
   };
   inputConfirmPassword.addEventListener("change", testPasswords);
-  
+
   inputNewsletter.addEventListener("change", () => {
     if (inputNewsletter.checked) {
       alert("Recibirás todas nuestras novedades");
@@ -58,10 +61,10 @@ try {
       alert("No recibirás nuestras novedades");
     }
   });
-  
+
   const submitData = () => {
     if (
-    !inputFirstName.value ||
+      !inputFirstName.value ||
       !inputLastName.value ||
       !inputUsername.value ||
       !inputPassword.value ||
@@ -74,7 +77,7 @@ try {
       window.location.reload();
     }
   };
-  buttonSubmit.addEventListener("click", submitData); 
+  buttonSubmit.addEventListener("click", submitData);
 } catch (error) {
-  console.error(error)
+  console.error(error);
 }
